467189347d2dec76b2d6e9e48a4e91d147173ba8
dat02092002 <ntdat02092002@gmail.com>
2022-04-19 11:36:30 +0700

    del hieu tai sao cai .env ngu vcl

--- models/db-conncect.js
+++ models/db-conncect.js
@@ -5,8 +5,8 @@ const con = mysql.createPool({
     connectionLimit : 10, // default = 10
     host: process.env.HOST,
     port: process.env.PORT,
-    user: process.env.USER,
-    //user: 'root',
+    //user: process.env.USER,
+    user: 'sql10486696',
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 });

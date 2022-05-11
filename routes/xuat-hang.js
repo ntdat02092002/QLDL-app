d6ed492dbd0bedc9688488776390c8377c684d36
dat02092002 <ntdat02092002@gmail.com>
2022-05-11 09:53:57 +0700

    insert data into db after submit phieu xuat hang

--- routes/xuat-hang.js
+++ routes/xuat-hang.js
@@ -37,8 +37,25 @@ router.post('/submit', basic.check(async (req,res) => {
     const data = req.body;
     console.log(data);
     const conn = await connection().catch(e => res.send('Sorry! Something went wrong.'));
+    const sql_1 = "INSERT INTO PHIEUXUATHANG (MaDaiLy, NgayLapPhieu, TongTien) VALUES ?";
+    const value = [[parseInt(data.MaDaiLy), data.NgayLapPhieu, parseInt(data.TongTien)]];
+    const result = await query(conn, sql_1, [value])
+        .catch(e => res.send('Sorry! Something went wrong.'));
+    const MaPhieuXuat = result.insertId;
 
-    const sql_1 = "";
+    let arrayOfCT_PXH = [];
+    data.MaMatHang.forEach((_, index) => {
+        let MaMatHang = parseInt(data.MaMatHang[index]);
+        let SoLuongXuat = parseInt(data.SoLuong[index]);
+        let DonGiaXuat = parseInt(data.DonGia[index]);
+        let ThanhTien = parseInt(data.ThanhTien[index]);
+        arrayOfCT_PXH.push([MaPhieuXuat, MaMatHang, SoLuongXuat, DonGiaXuat, ThanhTien]);
+    })
+    console.log(arrayOfCT_PXH);
+    const sql_2 = "INSERT INTO CT_PXH (MaPhieuXuat, MaMatHang, SoLuongXuat, DonGiaXuat, ThanhTien) VALUES ?";
+    const result_2 = await query(conn, sql_2, [arrayOfCT_PXH])
+        .catch(e => {res.send('Sorry! Something went wrong.'); console.log(e)});
+    res.send("ok");
 
 }));
 

83da0142819faaac26281cb174d926f8afe423b7
dat02092002 <ntdat02092002@gmail.com>
2022-05-13 19:17:27 +0700

    add thay-doi-tham-so feature

--- routes/quy-dinh.js
+++ routes/quy-dinh.js
@@ -14,8 +14,42 @@ const basic = auth.basic({
 
 router.get('/', basic.check((req, res) => {
 
-    res.send("chua co gi het~");
+    res.render('quy_dinh/quy-dinh');
 }));
 
+router.get('/tham-so', basic.check(async (req, res) => {
+    const conn = await connection().catch(e => {});
+    const sql = "SELECT * FROM THAMSO"
+    const DbResponse = await query(conn, sql)
+        .catch(e => res.send('Sorry! Something went wrong.'));
+
+    const TyLeXuat = DbResponse.filter(data => data.TenThamSo === "TyLeXuat")[0].GiaTri;
+    const DaiLyToiDaMoiQuan = DbResponse.filter(data => data.TenThamSo === "DaiLyToiDaMoiQuan")[0].GiaTri;
+    const ThuVuotNo = DbResponse.filter(data => data.TenThamSo === "ThuVuotNo")[0].GiaTri;
+
+    conn.end();
+    res.render('quy_dinh/tham_so/tham-so', {TyLeXuat: TyLeXuat, DaiLyToiDaMoiQuan: DaiLyToiDaMoiQuan, ThuVuotNo:ThuVuotNo});
+}));
+
+router.post('/tham-so', basic.check(async (req, res) => {
+    const data = req.body;
+
+    const TyLeXuat = parseInt(data.TyLeXuat);
+    const DaiLyToiDaMoiQuan = parseInt(data.DaiLyToiDaMoiQuan);
+    const ThuVuotNo = parseInt(data.ThuVuotNo);
+
+    const arrayOfGiaTri = [TyLeXuat, DaiLyToiDaMoiQuan, ThuVuotNo];
+    const arrayOfTenThamSo = ['TyLeXuat', 'DaiLyToiDaMoiQuan', 'ThuVuotNo'];
+
+    const conn = await connection().catch(e => {});
+    for(let i = 0; i < arrayOfGiaTri.length; i++){
+        let sql = "UPDATE THAMSO SET GiaTri = ? WHERE TenThamSo = ?";
+        await query(conn, sql, [arrayOfGiaTri[i], arrayOfTenThamSo[i]])
+            .catch(e => res.send('Sorry! Something went wrong.'));
+    }
+    conn.end();
+
+    res.render("quy_dinh/tham_so/tham-so-submitted");
+}));
 
 module.exports = router;

3f80b3aa370097c5dc1931cdede314ee6402cb07
PhucSon16 <106917525+PhucSon16@users.noreply.github.com>
2022-06-10 11:04:05 +0700

    Add files via upload

--- routes/dang-ki.js
+++ routes/dang-ki.js
@@ -1,21 +1,10 @@
-const express = require('express');
-
-const connection = require('../helpers/db-connection');
-const query = require('../helpers/db-query');
-const path = require('path');
-const auth = require('http-auth');
-const {check, validationResult} = require("express-validator");
-
-const router = express.Router();
-
-const basic = auth.basic({
-    file: path.join(__dirname, '../users.htpasswd'),
-});
-
-router.get('/', basic.check((req, res) => {
-
-    res.send("chua co gi het~");
-}));
-
-
-module.exports = router;
+function validate() {
+    var u = document.getElementById("username").value;   
+    if(u== "") {
+    alert("Vui lòng nhập tên!");
+    return false;
+    }   
+    alert("Xin hãy điền đúng thông tin!")
+     
+    return true;
+    }
